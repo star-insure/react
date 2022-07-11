1c7757be02a664ec433e30cc6dbb2c67ee10a928
alexclark_nz <hello@alexclark.co.nz>
2022-07-11 13:34:12 +1200

    Export errors component

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -2,10 +2,12 @@ import DateOfBirthField from './DateOfBirthField';
 import MoneyField from './MoneyField';
 import RegistrationSearchField from './RegistrationSearchField';
 import FormTester from './FormTester';
+import Errors from './Errors';
 
 export {
     DateOfBirthField,
     MoneyField,
     RegistrationSearchField,
     FormTester,
+    Errors,
 }
